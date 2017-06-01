e77718a35964db8c66d30a276c8641135bc79c07
Leif Bjornson <labjornson@gmail.com>
2017-06-01 11:40:06 -0500

    add nav menu

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,23 +1,43 @@
 import React from "react"
 
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Link, Route } from "react-router-dom"
+import { Button, Menu } from "semantic-ui-react"
 
 import ActivityList from "./ActivityList"
 import CreateActivity from "./CreateActivity"
 
-const App = () => (
-  <div className="ui main text container">
-    <h1 className="ui center aligned header">
-      <i className="compass icon" />
-      kidDO
-    </h1>
+const mainContainerStyle = {
+  marginTop: "6em"
+}
+
+const App = ({ history }) => {
+  return (
     <Router>
       <div>
-        <Route exact path="/" component={ActivityList} />
-        <Route path="/create-activity" component={CreateActivity} />
+        <Menu fixed="top" inverted>
+          <Menu.Item header>
+            <Link to="/">
+              <h1>
+                <i className="compass icon" />
+                kidDO
+              </h1>
+            </Link>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Link to="/create-activity" className="ui button primary">
+                Create activity
+              </Link>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <div className="ui main text container" style={mainContainerStyle}>
+          <Route exact path="/" component={ActivityList} />
+          <Route path="/create-activity" component={CreateActivity} />
+        </div>
       </div>
     </Router>
-  </div>
-)
+  )
+}
 
 export default App
